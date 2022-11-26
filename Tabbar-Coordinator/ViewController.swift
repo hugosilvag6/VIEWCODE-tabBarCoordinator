e4dd5e3323fdcde4a8c58bb3867d5898e18de804
hugosilvag6 <hugosilvag6@hotmail.com>
2022-11-26 18:23:16 -0300

    Initial Commit

---
+++ Tabbar-Coordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tabbar-Coordinator
+//
+//  Created by Hugo Silva on 26/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
---
+++ Tabbar-CoordinatorUITests/Tabbar_CoordinatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tabbar_CoordinatorUITestsLaunchTests.swift
+//  Tabbar-CoordinatorUITests
+//
+//  Created by Hugo Silva on 26/11/22.
+//
+
+import XCTest
+
+final class Tabbar_CoordinatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
